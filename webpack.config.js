8dc5ecd3c07885d07d78515cee856c2f1821accd
antonin <aleissen@beable.fr>
2021-01-20 14:31:45 +0100

    fix: change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -125,7 +125,7 @@ module.exports = () => {
             new CopyWebpackPlugin(
                 [
                     {
-                        from: './resources/',
+                        from: './src/',
                         to: './'
                     }
                 ]
